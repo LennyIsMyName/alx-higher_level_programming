c83ec335e20952cae9366693e102d99917a90721
LennyIsMyName <lennyofficial2001@gmail.com>
2023-03-07 17:40:53 +0300

    last digit pos or neg

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,15 +3,13 @@ import random
 number = random.randint(-10000, 10000)
 ten = 10
 last = number % ten
+a = "Last digit of"
 if number < 0:
     last = -abs(last)
 
 if last > 5:
-    print("Last digit of {} is {} and\
-            is greater than 5".format(number, last))
+    print("{} {} is {} and is greater than 5".format(a, number, last))
 elif last == 0:
-    print("Last digit of {} is 0 and is 0".format(number))
+    print("{} {} is 0 and is 0".format(a, number))
 elif last < 6 and not 0:
-    print("Last digit of {} is {} and\
-            is less than 6 and not 0".format(number, last))
-
+    print("{} {} is {} and is less than 6 and not 0".format(a, number, last))
