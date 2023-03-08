c76317536ef2139b923a1cf6b69b4a14f0c211da
LennyIsMyName <lennyofficial2001@gmail.com>
2023-03-08 17:28:17 +0300

    change a string to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+     for i in str:
+         print("{}".format(chr(ord(i) - 32) if (ord(i) >= 97 and ord(i) <= 122) else i), end='')
+     print()
+
+
+
+'''
+
     if ord(i) >= 97 and ord(i) <= 122:
         print(chr(ord(i) - 32), end="")
     else:
         print("{}".format(i), end="")
     print()
+'''
