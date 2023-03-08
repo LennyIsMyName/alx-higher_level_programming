95b0ddabd8e65c06eb23cc8aaa8239415f154f49
LennyIsMyName <lennyofficial2001@gmail.com>
2023-03-08 11:31:45 +0300

    print numbers in decimal and hexa

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -2,6 +2,6 @@
 
 # print number 1 to 98 in decimal and hexa
 
-#loop through 1 to 98.
-for i in range(1, 99):
+# loop through 1 to 98.
+for i in range(0, 99):
     print("{} = {}".format(i, hex(i)))
