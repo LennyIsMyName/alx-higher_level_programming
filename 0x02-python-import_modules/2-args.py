1bde4bdb3e34fc84ce73e75093066d9dfe38ddd5
LennyIsMyName <lennyofficial2001@gmail.com>
2023-03-11 13:05:26 +0300

    2 correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
 # print command line arguments and their positions
-num = len(sys.argv)
-if num == 1:
-    print('{} arguments.'.format(num - 1))
-elif num == 2:
-    print('{} argument:'.format(num - 1))
-    print('{}: {}'.format(1, sys.argv[1]))
-else:
-    print('{} arguments:'.format(num - 1))
-    for i in range(1, num):
-        print('{}: {}'.format(i, sys.argv[i]))
+if __name__ == '__main__':
+    num = len(sys.argv)
+    if num == 1:
+        print('{} arguments.'.format(num - 1))
+    elif num == 2:
+        print('{} argument:'.format(num - 1))
+        print('{}: {}'.format(1, sys.argv[1]))
+    else:
+        print('{} arguments:'.format(num - 1))
+        for i in range(1, num):
+            print('{}: {}'.format(i, sys.argv[i]))
