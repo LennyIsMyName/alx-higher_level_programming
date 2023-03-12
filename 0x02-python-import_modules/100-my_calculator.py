7ecb33b3d5668cfe5ad988011b667d342b7a47a0
LennyIsMyName <lennyofficial2001@gmail.com>
2023-03-12 10:42:18 +0300

    lil calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,9 @@ if __name__ == '__main__':
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     sign = sys.argv[2]
-
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
     if sign != '*' or sign != '/' or sign != '+' or sign != '-':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
