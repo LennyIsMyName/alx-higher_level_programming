fdb2bf1e222ef9761dc41f1b7966aa8a6647bef7
LennyIsMyName <lennyofficial2001@gmail.com>
2023-03-13 08:33:26 +0300

    print list items

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,4 @@
 
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
-        if type(my_list[i]) == int:
-            print(my_list[i])
+            print('{}'.format(my_list[i]))
