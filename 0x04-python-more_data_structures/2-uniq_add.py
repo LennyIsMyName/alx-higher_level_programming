07562ed773765b3c13956d2f299763d5c52124ba
LennyIsMyName <lennyofficial2001@gmail.com>
2023-03-15 20:35:25 +0300

    add unique numbers

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
+    setIt = set(my_list)
+    ans = 0
+    for i in setIt:
+        ans = ans + i
+    return ans
+
+
+
+    '''
     ans = 0
     for num in my_list:
         if my_list.count(num) > 1:
@@ -8,3 +17,4 @@ def uniq_add(my_list=[]):
     for i in my_list:
         ans = ans + i
     return ans
+    '''
