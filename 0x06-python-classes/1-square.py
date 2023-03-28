e205cb6158ee1a1cdd23a0f42b25e58e76daf130
LennyIsMyName <lennyofficial2001@gmail.com>
2023-03-28 22:33:54 +0300

    private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" put some data to the class"""
+class Square:
+    """ square class """
+    def __init__(self, size):
+        """ initializing the instance
+        Args:
+            size (int): size of the square
+            """
+        self.__size = size
