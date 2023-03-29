918f00e25bd4443577c462e12ffb6c35c09b2131
LennyIsMyName <lennyofficial2001@gmail.com>
2023-03-29 19:51:02 +0300

    square: getter and setter

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,25 +3,16 @@
 """ size validation """
 
 class Square:
+    """ The square class """
+
     def __init__(self, size=0):
-        """ instances size. 
+        """ init a new square class. 
         Args:
             size (int): size to validate.
             """
-        self.__size = size
 
-    try:
-        def check_type(self):
-            if isinstance(self.__size, int):
-                if (self.__size < 0):
-                    raise ValueError("size must be >= 0")
-            else:
-                raise TypeError("size must be an integer")
-    except TypeError:
-        def geterr():
-            return "size must be an integer"
-        geterr()
-    except ValueError:
-        def geterr():
-            return "size must be >= 0"
-        geterr()
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
