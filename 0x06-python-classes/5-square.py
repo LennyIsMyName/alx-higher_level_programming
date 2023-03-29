a1c023c0a12e389a8123af49cef262667a92208c
LennyIsMyName <lennyofficial2001@gmail.com>
2023-03-29 21:47:46 +0300

    print a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+""" size validation of Square"""
+
+class Square:
+    """ The square class """
+
+    def __init__(self, size=0):
+        """ init a new square class. 
+        Args:
+            size (int): size to validate.
+            """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+    def area(self):
+        """ returns the area of the current square"""
+        var = self.__size * self.__size
+        return var
+
+    def my_print(self):
+        if self.__size:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end='') 
+                print()
+        else:
+            print()
+            print()
