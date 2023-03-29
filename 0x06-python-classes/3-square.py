8a60159475266daf6adb0e5e5a55d4d9f394d76c
LennyIsMyName <lennyofficial2001@gmail.com>
2023-03-29 20:28:37 +0300

    Public instance method

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
 
-""" Area of a square """
+""" size validation of Square"""
 
 class Square:
+    """ The square class """
+
     def __init__(self, size=0):
-        """ instances size. 
+        """ init a new square class. 
         Args:
             size (int): size to validate.
             """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
-    try:
-        def check_type(self):
-            if isinstance(self.__size, int):
-                if (self.__size < 0):
-                    raise ValueError("size must be >= 0")
-            else:
-                raise TypeError("size must be an integer")
-    except TypeError:
-        def geterr():
-            return "size must be an integer"
-        geterr()
-    except ValueError:
-        def geterr():
-            return "size must be >= 0"
-        geterr()
-
     def area(self):
-        """ returns the square of a number """
-        area = self.__size * self.__size
-        return (area)
+        """ returns the area of the current square"""
+        var = self.__size * self.__size
+        return var
+        
+
