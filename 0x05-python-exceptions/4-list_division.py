44a4bc35e82af7583fca71a93007b4c62b99188f
LennyIsMyName <lennyofficial2001@gmail.com>
2023-03-30 16:52:29 +0300

    divide element by element in 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,6 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             ans = my_list_1[i] / my_list_2[i]
             new_list.append(ans)
-        
         except TypeError:
             print("wrong type")
             new_list.append(0)
