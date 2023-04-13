8251d6910fb5bdc7c6857fc177e952f177b642ad
LennyIsMyName <lennyofficial2001@gmail.com>
2023-04-13 10:23:46 +0300

    read a json file and change it to python obj

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" return obj from json file"""
+import json
+
+
+def load_from_json_file(filename):
+    """ return obj from json file"""
+    with open(filename, mode="r", encoding="utf-8") as file:
+        """ return obj from json file"""
+        jsnOb = file.read()
+        obj = json.loads(jsnObj)
+        return obj
