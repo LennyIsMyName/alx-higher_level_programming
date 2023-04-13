324b01bdd344ef628c6191a39b883b0c4731475f
LennyIsMyName <lennyofficial2001@gmail.com>
2023-04-13 10:03:07 +0300

    json to python object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""change json to python object"""
+import json
+
+"""change json to python object """
+
+
+def from_json_string(my_obj):
+    """change json to python object """
+    jsn = json.loads(my_obj)
+    return jsn
