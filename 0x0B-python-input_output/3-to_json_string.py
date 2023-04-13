055623ddda92344330b62c94e81797cb03d4f775
LennyIsMyName <lennyofficial2001@gmail.com>
2023-04-13 09:51:43 +0300

    change string to json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-
 import json
 
 
-""" JSON representation of an object """
-
-
 def to_json_string(my_obj):
-    """ converts an object to JSON """
     jsn = json.dumps(my_obj)
     return jsn
