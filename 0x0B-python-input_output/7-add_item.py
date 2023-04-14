1bd44bb8fb3320b3830df4b9ba1e901765fd8b8b
LennyIsMyName <lennyofficial2001@gmail.com>
2023-04-14 11:50:07 +0300

    add cl args to list, then to json file as json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,17 @@
 import sys
 import json
 from os import path
-
+""" add the object as json"""
 to = __import__('5-save_to_json_file').save_to_json_file
+
+""" get the json object """
 fro = __import__('6-load_from_json_file').load_from_json_file
 
 
 def add_item(args: list[str]):
-    ''' add, load and then save to a file '''
+    """
+    add, load and then save to a file 
+    """
     file = "add_item.json"
 
     if path.isfile(file):
