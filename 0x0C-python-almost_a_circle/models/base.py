efc0f54e797a9b33c6d130d33c6ba0cab6ad38e2
LennyIsMyName <lennyofficial2001@gmail.com>
2023-04-19 16:56:12 +0300

    save string representation of a class to a json file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,9 +16,20 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ return json representation of class """
         if list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ saves json representation of classes to json files """
+        name = cls.__name__+".json"
+        li = []
+        for item in list_objs:
+            li.append(item.to_dictionary())
+        with open(name, "w") as file:
+            file.write(cls.to_json_string(li))
