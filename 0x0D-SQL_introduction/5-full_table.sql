4f01a98e65e5ea21873fc4752812836ac61d0a5e
LennyIsMyName <lennyofficial2001@gmail.com>
2023-05-18 16:55:05 +0300

    show the whole table description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,6 @@
 -- Prints a table description.
 
-SHOW COLUMNS FROM hbtn_0c_0.first_table;
+SELECT COLUMN_NAME, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_TYPE, COLUMN_KEY
+FROM hbtn_0c_0.first_table;
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+AND TABLE_NAME = 'first_name';
