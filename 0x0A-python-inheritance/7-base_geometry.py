5ba78bf3d08d5d4592c72e2aca05e33d50bdfe6c
LennyIsMyName <lennyofficial2001@gmail.com>
2023-06-13 12:42:00 +0300

    the second largest of argvs

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,8 +16,13 @@ class BaseGeometry():
 
     def integer_validator(self, name, value):
         self.name = name
-        self.value = value
         if not isinstance(self.value, int):
             raise TypeError(f"{name} must be an integer")
         if self.value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+        self.value = value
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("test/7-base_geometry.txt")
+
