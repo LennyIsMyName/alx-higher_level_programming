793e859c8b8690fce25e2cc88bc58bf28bdfc9ea
LennyIsMyName <lennyofficial2001@gmail.com>
2023-06-13 04:47:05 +0300

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]) || 1;
+
+const factorial = (n) => {
+  if (n === 0) {
+    return 1;
+  } else {
+    return (n * factorial(n - 1));
+  }
+};
+
+console.log(factorial(num));
