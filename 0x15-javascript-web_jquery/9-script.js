519319f883910675f56233b836713af6f3ae5792
LennyIsMyName <lennyofficial2001@gmail.com>
2023-08-07 15:44:34 +0300

    error fetching content

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,9 +1,13 @@
 // Get a value of a given key from a link to json.
 
 $(document).ready(function () {
-	const url = "https://fourtonfish.com/hellosalut/?lang=fr";
-	$.getJSON(url, function(data) {
-		const hello = data.hello;
-		$("#hello").text(hello);
-	});
+  const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
+  $.getJSON(url)
+    .done(function (data) {
+      const hello = data.hello;
+      $('#hello').text(hello);
+    })
+    .fail(function () {
+      $('#hello').text('error fetching content.');
+    });
 });
