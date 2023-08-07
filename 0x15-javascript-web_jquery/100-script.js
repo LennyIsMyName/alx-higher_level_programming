55df5dfe82da53d4a8a07212380b5682029c6a75
LennyIsMyName <lennyofficial2001@gmail.com>
2023-08-07 16:00:05 +0300

    vanilla js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+/* Change the color of the text in vanilla js */
+
+document.addEventListener('DOMContentLoaded', function () {
+  const el = document.querySelector('header');
+  el.style.color = '#FF0000';
+});
