b7b32b8f0bb1cac850855ad444738af536168131
LennyIsMyName <lennyofficial2001@gmail.com>
2023-09-24 12:06:46 +0300

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const https = require('https');
+const request = require('request');
 const arg = process.argv.slice(2);
 const url = arg[0];
-https.get(url, (response) => {
+request.get(url, (response) => {
   console.log(`code: ${response.statusCode}`);
 });
